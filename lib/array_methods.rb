76c52a4865b2f9b03b22e34d487b2ece5987d8ac
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 17:47:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse(array)
 end
 
 def using_first(array)
-
+  array.first(array)
 end
 
 def using_last(array)
-
+  array.last(array)
 end
 
 def using_size(array)
-
+  array.size(array)
 end
